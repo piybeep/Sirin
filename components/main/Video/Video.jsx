c002738ff92cb342f861747bf432e1d79b778389
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-04-30 15:09:03 +0300

    feat: добавил ссылку на трансляцию

--- components/main/Video/Video.jsx
+++ components/main/Video/Video.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React from "react";
 
 // Style
-import s from './Video.module.scss'
+import s from "./Video.module.scss";
 
 const Video = () => {
-    return (
-        <div className={s.video}>
-           <iframe className={s.video__iframe} width="1864" height="808" src="https://www.youtube.com/embed/ajnkfYxqWjM" title="Shelby Cobra... Миша, ты серьёзно?" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-        </div>
-    );
+	return (
+		<div className={s.video}>
+			<iframe
+				className={s.video__iframe}
+				width="1864"
+				height="808"
+				src="https://www.youtube.com/embed/N9Slx5bHF8s"
+				title="Танцевальный спектакль | Царевна-лягушка"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				allowfullscreen
+			></iframe>
+		</div>
+	);
 };
 
 export default Video;
--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,88 @@
 // Components
-import Header from '../components/main/Header/Header'
-import Preview from '../components/main/Preview/Preview'
-import About from '../components/main/About/About'
-import Advantages from '../components/main/Advantages/Advantages'
-import Rules from '../components/main/Rules/Rules'
-import Video from '../components/main/Video/Video'
-import Team from '../components/main/Team/Team'
-import Reviews from '../components/main/Reviews/Reviews'
-import Contact from '../components/main/Contacts/Contact'
-import Footer from '../components/Footer/Footer'
-import Menu from '../components/main/Menu/Menu'
+import Header from "../components/main/Header/Header";
+import Preview from "../components/main/Preview/Preview";
+import About from "../components/main/About/About";
+import Advantages from "../components/main/Advantages/Advantages";
+import Rules from "../components/main/Rules/Rules";
+import Video from "../components/main/Video/Video";
+import Team from "../components/main/Team/Team";
+import Reviews from "../components/main/Reviews/Reviews";
+import Contact from "../components/main/Contacts/Contact";
+import Footer from "../components/Footer/Footer";
+import Menu from "../components/main/Menu/Menu";
 // Form
-import Form from '../components/Form/Form'
-import { Provider } from 'react-redux'
-import { setupStore } from '../src/store/store'
+import Form from "../components/Form/Form";
+import { Provider } from "react-redux";
+import { setupStore } from "../src/store/store";
 
 // for SSR
-import { wrapper } from '../src/store/store'
-import { fetchAllTeam, teamAPI } from '../src/team/teamService'
-import { contactsAPI, fetchContact } from '../src/contacts/contacts'
-import { getReviews, reviewsAPI } from '../src/reviews/reviews'
+import { wrapper } from "../src/store/store";
+import { fetchAllTeam, teamAPI } from "../src/team/teamService";
+import { contactsAPI, fetchContact } from "../src/contacts/contacts";
+import { getReviews, reviewsAPI } from "../src/reviews/reviews";
 
 // SSR for team
 export const getServerSideProps = wrapper.getServerSideProps(
-  (store) => async (context) => {
-    store.dispatch(fetchAllTeam.initiate())
-    const [team] = await Promise.all(store.dispatch(teamAPI.util.getRunningQueriesThunk()))
+	(store) => async (context) => {
+		store.dispatch(fetchAllTeam.initiate());
+		const [team] = await Promise.all(
+			store.dispatch(teamAPI.util.getRunningQueriesThunk()),
+		);
 
-    store.dispatch(fetchContact.initiate())
-    const [contacts] = await Promise.all(store.dispatch(contactsAPI.util.getRunningQueriesThunk()))
+		store.dispatch(fetchContact.initiate());
+		const [contacts] = await Promise.all(
+			store.dispatch(contactsAPI.util.getRunningQueriesThunk()),
+		);
 
-    store.dispatch(getReviews.initiate())
-    const [reviews] = await Promise.all(store.dispatch(reviewsAPI.util.getRunningQueriesThunk()))
+		store.dispatch(getReviews.initiate());
+		const [reviews] = await Promise.all(
+			store.dispatch(reviewsAPI.util.getRunningQueriesThunk()),
+		);
 
-    return {
-      props: {
-        team: !team.isError && team.data, contacts: !contacts.isError && contacts.data, reviews: !reviews.isError && reviews.data,
-        teamError: team.isError && team.error, contactsError: contacts.isError && contacts.error, reviewsError: contacts.isError && contacts.error
-      }
-    }
-  }
-)
+		return {
+			props: {
+				team: !team.isError && team.data,
+				contacts: !contacts.isError && contacts.data,
+				reviews: !reviews.isError && reviews.data,
+				teamError: team.isError && team.error,
+				contactsError: contacts.isError && contacts.error,
+				reviewsError: contacts.isError && contacts.error,
+			},
+		};
+	},
+);
 
-export default function Home({ team, contacts, reviews, teamError, contactsError, reviewsError }) {
-  return (
-    <div className='component'>
-      <Menu />
-      <Preview />
-      <About />
-      <Advantages />
-      <Rules />
-      {/* <Video /> */}
-      <Team team={team} error={teamError}/>
-      <Reviews reviews={reviews} error={reviewsError}/>
-      <Contact contacts={contacts} error={contactsError} />
-    </div>
-  )
+export default function Home({
+	team,
+	contacts,
+	reviews,
+	teamError,
+	contactsError,
+	reviewsError,
+}) {
+	return (
+		<div className="component">
+			<Menu />
+			<Preview />
+			<About />
+			<Advantages />
+			<Rules />
+			<Video />
+			<Team team={team} error={teamError} />
+			<Reviews reviews={reviews} error={reviewsError} />
+			<Contact contacts={contacts} error={contactsError} />
+		</div>
+	);
 }
 
 Home.getLayout = function PageLayout(page) {
-  const store = setupStore()
-  return (
-    <Provider store={store}>
-      <Header />
-      <Form />
-      {page}
-      <Footer />
-    </Provider>
-  )
-}
+	const store = setupStore();
+	return (
+		<Provider store={store}>
+			<Header />
+			<Form />
+			{page}
+			<Footer />
+		</Provider>
+	);
+};
