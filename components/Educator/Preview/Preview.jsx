42fc4f3cf294451ad305d2088aa319b4e23eebd7
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-08-15 21:31:24 +0300

    fix: баннер * Добавил баннер на страницу участника команты

--- components/Educator/Preview/Preview.jsx
+++ components/Educator/Preview/Preview.jsx
@@ -5,13 +5,15 @@ import error from '../../../public/images/team/errorTeamMini.png'
 import s from './Preview.module.scss'
 
 const Preview = ({ personInfo }) => {
+
+    console.log(personInfo)
     return (
-        <div className={s.preview}>
+        <div className={s.preview} style={{ backgroundImage: personInfo?.banner[0]?.filename && `url(${process.env.NEXT_PUBLIC_STATIC_URL + personInfo?.banner[0]?.filename})` }}>
             <div className={s.preview__info}>
                 {
                     personInfo ?
                         <>
-                            < img className={s.preview__img} src={personInfo?.photo[0]?.filename ? process.env.NEXT_PUBLIC_STATIC_URL + personInfo.photo[0].filename : error.src} alt="" />
+                            <img className={s.preview__img} src={personInfo?.photo[0]?.filename ? process.env.NEXT_PUBLIC_STATIC_URL + personInfo.photo[0].filename : error.src} alt="" />
                             <h2 className={s.preview__title}>{personInfo.fullname}</h2>
                             <p className={s.preview__text}>
                                 <span className={s.preview__text}>{personInfo.vacancy}</span>
